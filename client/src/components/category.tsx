814b5c605db2f253b8291a80e65d11afe8de15e7
Shahnaaz Rahamatullah <rahamsha@gmail.com>
2024-01-05 15:04:49 +0000

    test: add tests for category component

--- client/src/components/category.tsx
+++ client/src/components/category.tsx
@@ -1,12 +1,16 @@
-// Category.tsx
 import React from 'react';
 
 type CategoryProps = {
-  category: string;
+  category?: string;
 };
 
-const Category: React.FC<CategoryProps> = ({ category}) => {
-  return <h1 className="text-center text-6xl py-10">{category}</h1>;
+const Category: React.FC<CategoryProps> = ({ category }) => {
+  return (
+    <>
+      {category && <h1 className="text-center text-6xl py-10">{category}</h1>}
+    </>
+  );
 };
 
 export default Category;
+

852101bb25b21e1a3c9f9aa8417462f4559e98b3
Renata Giera-Dubiel <renatagiera@gmail.com>
2024-01-09 22:15:12 +0000

    fix: change group name

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer: React.FC = () => (
   <footer className="max-w-screen-lg mx-auto py-2" data-testid="footer">
-    <div className="text-[0.6rem] mr-5 text-right"><span className="uppercase">Team A - Synergy: </span> Tech Returners Project</div>
+    <div className="text-[0.6rem] mr-5 text-right"><span className="uppercase">Group 01 - Synergy: </span> Tech Returners Project</div>
   </footer>
 );
 
